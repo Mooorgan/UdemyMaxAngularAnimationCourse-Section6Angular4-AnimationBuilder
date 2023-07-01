77a2a97ee54727497afa4d588a82c850d14b5f1a
Shaishav Paudel <morganhouse15@gmail.com>
2023-07-01 20:01:25 +0545

    AngularAnimationsUdemyMax-Section6-AnimationBuilderCompleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { animate, AnimationBuilder, style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularAnimationSection6-animationBuilder';
+  constructor(private builder: AnimationBuilder) {}
+
+  animate(element: any) {
+    const animation = this.builder.build([
+      style({
+        backgroundColor: 'red',
+        width: '200px'
+      }),
+      animate(300, style({
+        width: '500px'
+      })),
+      animate(200)
+    ]);
+
+    const player = animation.create(element);
+    player.play();
+  }
 }
